94944c1c7a70b14be5e7a7ce5354e3cfabf503bc
Kaicheng Zhang <kaichengz@gmail.com>
2014-06-25 10:41:39 -0500

    revise xhr transport

--- xhr.go
+++ xhr.go
@@ -14,17 +14,35 @@ func (xhr *XHR) InitXHR(req *Request) {
 	xhr.Polling.doWrite = func(req *Request, data []byte) {
 		debug(fmt.Sprintf("xhr writing \"%s\"", string(data)))
 		contentType := "text/plains; charset=UTF-8"
+		if (data[0] < 20) {
+			contentType = "application/octet-stream"
+		}
 		contentLength := fmt.Sprintf("%d", len(data))
 		res := req.res
 		res.Header().Set("Content-Type", contentType)
 		res.Header().Set("Content-Length", contentLength)
+
+		// TODO: Prevent XSS warning on IE.
+
 		xhr.headers(req)
 		res.WriteHeader(200)
 		res.Write(data)
 	}
 
 	xhr.Polling.headers = func(req *Request) {
-		xhr.Emit("headers", req.res.Header())
+		httpreq := req.httpReq
+		res := req.res
+
+		origin := httpreq.Header.Get("Origin")
+
+		if (len(origin) > 0) {
+			res.Header.Set("Access-Control-Allow-Credentials", "true")
+			res.Header.Set("Access-Control-Allow-Origin", origin)
+		} else {
+			es.Header.Set("Access-Control-Allow-Origin", "*")
+		}
+
+		xhr.Emit("headers", res)
 	}
 }
 
